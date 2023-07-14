b75ab7fe286f1a7ff86819594bb9c1013c82219c
Paxy <petar.bojovic.paxy@gmail.com>
2023-07-14 17:42:26 +0200

    Update MQTTClient.java

--- MQTTClient.java
+++ MQTTClient.java
@@ -1,82 +1,138 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.sql.Timestamp;
+import java.util.UUID;
+import java.util.concurrent.Callable;
 
-public class ModbusClient implements Runnable {
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MQTTClient implements Runnable, MqttCallback {
 
     private Engine engine;
-    private Socket srv;
+    private IMqttClient client;
+    private String prefix;
 
-    public ModbusClient(Engine engine) throws IOException {
+    public static final String chargeSolarOnly = "40630001000A05040506139900031CE4";
+    public static final String chargeSolarUtility = "48E30001000A0504050613990002DD24";
+    public static final String loadUtility = "490A0001000A05040506139A0000ACE5";
+    public static final String loadSBU = "490D0001000A05040506139A00022D24";
+
+    public MQTTClient(Engine engine) throws Exception {
         this.engine = engine;
+        this.prefix = engine.mqttTopic;
+        connect();
+
     }
 
     public void run() {
-        while (true) {
-            try {
-                boolean close=false;
-                srv=new Socket(engine.realModbusServer,502);
-                String time = new Timestamp(System.currentTimeMillis()).toString();
-                System.out.println(time + " - Connected to server "
-                        + srv.getInetAddress().getHostAddress());
-                InputStream in=srv.getInputStream();
-                boolean first=true;
-                int cnt=0;
-                while (true) {
-                    while (in.available() == 0) {
-                        Thread.currentThread().sleep(100);
-                        if(first) cnt++;
-                        if(cnt>50) {
-                            close=true;
-                            break;
-                        }
-                    }
-                    first=false;
-                    if(close) {
-                        srv.close();
-                        time = new Timestamp(System.currentTimeMillis()).toString();
-                        System.out.println(time + "Server not responding, restarting connection.");
-                        break;
-                    }
-                    byte[] data=in.readNBytes(in.available());
-                    String hex=Engine.bytesToHex(data);
-                    time = new Timestamp(System.currentTimeMillis()).toString();
-                    System.out.println(time+" - Server: "+hex);
-                    int ret=engine.nsrv.sendData(data);
-                    if(ret==-1) break;
-                }
 
+    }
 
-            } catch (Exception e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+    public void connect() {
+        try {
+            String publisherId = UUID.randomUUID().toString();
+            MemoryPersistence persistence = new MemoryPersistence();
+            this.client = new MqttClient(
+                    "tcp://" + engine.mqttServer + ":" + engine.mqttPort,
+                    publisherId, persistence);
+            MqttConnectOptions options = new MqttConnectOptions();
+            if (engine.enableMqttAuth) {
+                options.setUserName(engine.mqttUser);
+                options.setPassword(engine.mqttPass.toCharArray());
             }
+            options.setAutomaticReconnect(true);
+            options.setCleanSession(true);
+            options.setConnectionTimeout(10);
+            client.connect(options);
+            while (!client.isConnected())
+                Thread.currentThread().sleep(100);
+            String time = new Timestamp(System.currentTimeMillis()).toString();
+            System.out.println(time + " - MQTT connected");
+            client.setCallback(this);
+            client.subscribe(engine.mqttTopic + "Set/#");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public int sendData(byte[] data) throws InterruptedException {
-        if(this.srv==null) {
-            String time = new Timestamp(System.currentTimeMillis()).toString();
-            System.out.println(time + " - Waiting for server ...");
-            while(srv==null) Thread.currentThread().sleep(100);
+    public void sendMsg(String topic, String msg) {
+        if (client == null || !client.isConnected()) {
+            connect();
+            return;
         }
         try {
-            if(this.srv.getOutputStream()==null) return -1;
-
-        OutputStream out=this.srv.getOutputStream();
-        out.write(data);
-        out.flush();
-
-        } catch (IOException e) {
+            MqttMessage mmsg = new MqttMessage(msg.getBytes());
+            client.publish(this.prefix + topic, mmsg);
+        } catch (MqttException e) {
             e.printStackTrace();
-            return -1;
         }
-        return 0;
+    }
+
+    public void sendMsg(String topic, int val) {
+        sendMsg(topic, String.valueOf(val));
+    }
+
+    public void sendMsg(String topic, double val) {
+        sendMsg(topic, String.valueOf(val));
+    }
+
+    @Override
+    public void connectionLost(Throwable arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void messageArrived(String topic, MqttMessage message)
+            throws Exception {
+        String msg = new String(message.getPayload());
+
+        if (topic.contains("chargeState")) {
+            if (msg.equals("3")) {
+                engine.nsrv.sendData(
+                        engine.hexStringToByteArray(this.chargeSolarOnly));
+                String time = new Timestamp(System.currentTimeMillis())
+                        .toString();
+                System.out.println(time + " - Server: " + chargeSolarOnly);
+            } else {
+                engine.nsrv.sendData(
+                        engine.hexStringToByteArray(this.chargeSolarUtility));
+                String time = new Timestamp(System.currentTimeMillis())
+                        .toString();
+                System.out.println(time + " - Server: " + chargeSolarUtility);
+            }
+        } else if (topic.contains("loadState")) {
+            if (msg.equals("2")) {
+                engine.nsrv.sendData(engine.hexStringToByteArray(this.loadSBU));
+                String time = new Timestamp(System.currentTimeMillis())
+                        .toString();
+                System.out.println(time + " - Server: " + loadSBU);
+            } else {
+                engine.nsrv.sendData(
+                        engine.hexStringToByteArray(this.loadUtility));
+                String time = new Timestamp(System.currentTimeMillis())
+                        .toString();
+                System.out.println(time + " - Server: " + loadUtility);
+            }
+        } else {
+            String time = new Timestamp(System.currentTimeMillis()).toString();
+            System.out.println("\nReceived a Message!" + "\n\tTime:    " + time
+                    + "\n\tTopic:   " + topic + "\n\tMessage: "
+                    + new String(message.getPayload()) + "\n\tQoS:     "
+                    + message.getQos() + "\n");
+        }
+
     }
 
 }
